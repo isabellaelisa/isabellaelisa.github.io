57e050c3febcf3a9228eca26efad20a461fa0b7c
isabellaelisa <ialvarado2@csustan.edu>
2025-05-07 09:18:00 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -155,7 +155,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'valentine',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
